02fedca8fd117532538fbdd52179337e28f02e80
EtienneD10 <etiennedalmas01@gmail.com>
2026-05-04 14:45:27 -0400

    implementing regd_users.delete

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -68,6 +68,27 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   res.status(200).json({ message:`Review for ISBN ${isbn} has been added/updated` })
 });
 
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+  const username = req.session.authorization.username;
+
+  // Check the book exist
+  if (!books[isbn]) {
+    return res.status(404).json({ message:"Book not found" })
+  }
+
+  // Check the user actually has a review to delete
+  if (!books[isbn].reviews[username]) {
+    return res.status(404).json({ message:"No review found for this Customer" });
+  }
+
+  // Delete only this user's review
+  delete books[isbn].reviews[username];
+
+  res.status(200).json({ message:`Review by ${username} for ISBN ${isbn} has been deleted` })
+
+})
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
